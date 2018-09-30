2c9694bbfd5f7da89d3e278f2e0e05015031644a
t.abe@aegis.dip.jp <t.abe.aegis@gmail.com>
2018-09-30 07:47:47 +0000

    libsharakuを機能ごとにリポジトリ分離

---
+++ build/linux-builds/00-clear.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------
+#
+#  MIT License
+#  
+#  Copyright (c) 2017 Abe Takafumi
+#  
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#  
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#  
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE. *
+#
+# ----------------------------------------------------------------------------
+
+
+cd `dirname $0`
+readonly OBJ_PATH=`pwd`
+readonly DEF_LOGPATH=${OBJ_PATH}/result
+readonly DEF_DEPROYPATH=${OBJ_PATH}/deproy
+readonly BASE_PATH=${OBJ_PATH}/../../
+
+rm -rf ${DEF_LOGPATH}
+rm -rf ${DEF_DEPROYPATH}
+
+if [ -f ${BASE_PATH}libs/generic/Makefile ]; then
+	# lib‚ðƒNƒŠƒA
+	make -C ${BASE_PATH}libs/container clean
+	make -C ${BASE_PATH}libs/atomic clean
+fi
